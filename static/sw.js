95194ebbd5cb83ab1e7e1c7274c18fc6c7d8bb8b
myblog <cinechillz01@gmail.com>
2026-08-29 14:21:45 +0530

    remove adblock detection and content lock

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11680522
+    "domain": "3nbf4.com",
+    "zoneId": 11680530
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
